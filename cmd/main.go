072b9f108d9bedc68ef898fd5e18b83e4e6cd0b4
erel213 <erel2008@gmail.com>
2024-06-18 07:40:43 +0300

    set up the application

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"cmd/main.go/internal/application"
+	postgres_user_repository "cmd/main.go/internal/infrastructure/db/repository"
+	"cmd/main.go/internal/interfaces/router"
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
+	db_connection_string := os.Getenv("DATASOURCE_URL")
+	db, err := sql.Open("postgres", db_connection_string)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	//define repositories
+	userRepo := postgres_user_repository.NewPostgresUserRepository(db)
+
+	//define service
+	userService := application.NewUserService(userRepo)
+
+	//define router
+	userRouter := router.NewUserRouter(userService)
+
+	app := fiber.New()
+
+	app.Post("/user", userRouter.CreateUser)
 }
