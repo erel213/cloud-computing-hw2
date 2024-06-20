e261fe5c45117f307e12b03696ffd1168e70abf2
erel213 <erel2008@gmail.com>
2024-06-20 08:51:02 +0300

    Setup the new message router

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"cmd/main.go/internal/application"
-	postgres_user_repository "cmd/main.go/internal/infrastructure/db/repository"
+	postgresRepository "cmd/main.go/internal/infrastructure/db/repository"
 	"cmd/main.go/internal/interfaces/router"
 	"database/sql"
 	"log"
@@ -21,15 +21,19 @@ func main() {
 	defer db.Close()
 
 	//define repositories
-	userRepo := postgres_user_repository.NewPostgresUserRepository(db)
+	userRepo := postgresRepository.NewPostgresUserRepository(db)
+	messageRepo := postgresRepository.NewPostgresMessageRepository(db)
 
 	//define service
 	userService := application.NewUserService(userRepo)
+	messageServicce := application.NewMessageService(messageRepo, userRepo)
 
 	//define router
 	userRouter := router.NewUserRouter(userService)
+	messageRouter := router.NewMessageRouter(messageServicce)
 
 	app := fiber.New()
 
 	app.Post("/user", userRouter.CreateUser)
+	app.Post("/message", messageRouter.SendMessage)
 }
