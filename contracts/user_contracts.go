20cd6db80fe7764078a88104e10ae371ffe3ab4e
erel213 <erel2008@gmail.com>
2024-06-22 18:10:46 +0300

    application layer - user service
    add new method for the user service - block user
    
    define the contracts for the new method

--- contracts/user_contracts.go
+++ contracts/user_contracts.go
@@ -5,3 +5,13 @@ import "github.com/google/uuid"
 type CreateUserResponse struct {
 	UserId uuid.UUID `json:"user_id"`
 }
+
+type UserResponse struct {
+	UserId       uuid.UUID
+	BlockedUsers []uuid.UUID
+}
+
+type BlockUserRequest struct {
+	UserId        uuid.UUID
+	BlockedUserId uuid.UUID
+}
