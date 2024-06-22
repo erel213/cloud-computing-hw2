fdaefbeb6afb6470d95a55d79f498c1ed5e814fc
erel213 <erel2008@gmail.com>
2024-06-22 18:08:57 +0300

    Extend domain model user
    extend the definition of user domain model to contains []uuid blocked users
    in addition add functionality to add blocked users

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -2,13 +2,16 @@ package entity
 
 import (
 	"errors"
+	"fmt"
+	"slices"
 	"whatsapp-like/internal/appError"
 
 	"github.com/google/uuid"
 )
 
 type User struct {
-	UserId uuid.UUID
+	UserId       uuid.UUID
+	BlockedUsers []uuid.UUID
 }
 
 func NewUser() (*User, appError.AppError) {
@@ -21,4 +24,28 @@ func NewUser() (*User, appError.AppError) {
 	return &User{
 		UserId: userId,
 	}, nil
+}
+
+func (u *User) BlockUser(blockedUserId uuid.UUID) appError.AppError {
+
+	if slices.Contains(u.BlockedUsers, blockedUserId) {
+		return appError.ValidationError{Err: fmt.Errorf("user %s already block user %s", u.UserId, blockedUserId)}
+	}
+
+	u.BlockedUsers = append(u.BlockedUsers, blockedUserId)
+	return nil
+}
+
+func (u *User) FilterBlockedMessages(messages *[]Message) (*[]Message, appError.AppError) {
+	filteredMessage := make([]Message, 0)
+
+	for _, message := range *messages {
+		if slices.Contains(u.BlockedUsers, message.FromUser) {
+			continue
+		}
+
+		filteredMessage = append(filteredMessage, message)
+	}
+
+	return &filteredMessage, nil
 }
