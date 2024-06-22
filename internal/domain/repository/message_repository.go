e828d70faf72b0fe4d8f2984470ce9cb9eeb3cee
erel213 <erel2008@gmail.com>
2024-06-22 09:43:40 +0300

    Message repository
    Extend functionality by adding new method that retrieve messages for user

--- internal/domain/repository/message_repository.go
+++ internal/domain/repository/message_repository.go
@@ -3,8 +3,11 @@ package repository
 import (
 	"whatsapp-like/internal/appError"
 	"whatsapp-like/internal/domain/entity"
+
+	"github.com/google/uuid"
 )
 
 type MessageRepository interface {
 	CreateMessage(message *entity.Message) appError.AppError
+	GetMessagesForUser(userId uuid.UUID) ([]*entity.Message, appError.AppError)
 }
