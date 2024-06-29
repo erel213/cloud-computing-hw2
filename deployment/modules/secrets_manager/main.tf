5206b8b4cd548db02951abf2aafa5e1b878f3302
erel213 <erel2008@gmail.com>
2024-06-29 18:50:17 +0300

    secrets management
    refactor secrets names for more concrete names

--- deployment/modules/secrets_manager/main.tf
+++ deployment/modules/secrets_manager/main.tf
@@ -17,23 +17,25 @@ resource "random_password" "db_credentials_password" {
   override_special = "_%"
 }
 
-resource "aws_secretsmanager_secret" "db_credentials" {
+resource "aws_secretsmanager_secret" "db-password" {
   kms_key_id = aws_kms_key.secrets_manager.key_id
-  name = "db-credentials-password"
+  name = "db-password"
   description = "RDS Admin Password"
+  recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "db_credentials" {
-  secret_id = aws_secretsmanager_secret.db_credentials.id
+resource "aws_secretsmanager_secret_version" "db-password" {
+  secret_id = aws_secretsmanager_secret.db-password.id
   secret_string = random_password.db_credentials_password.result
 }
 
 resource "aws_secretsmanager_secret" "ecs_rds_connection_string" {
-  name = "ecs-rds-connection-string"
+  name = "rds-connection-string"
   description = "Connection string for RDS for ECS tasks"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "ecs_rds_connection_string" {
   secret_id = aws_secretsmanager_secret.ecs_rds_connection_string.id
-  secret_string = "postgresql://root:${aws_secretsmanager_secret_version.db_credentials.secret_string}@${var.rds_endpoint}:5432/whatsapp-like?sslmode=disable"
+  secret_string = "postgresql://root:${aws_secretsmanager_secret_version.db-password.secret_string}@${var.rds_endpoint}/whatsapp-like"
 }
