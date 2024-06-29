c1eac44ad460259039ff25f2d10ea97f5e294323
erel213 <erel2008@gmail.com>
2024-06-29 18:49:47 +0300

    application layer
    allow ingress from port 8080

--- deployment/modules/secuirty_groups/main.tf
+++ deployment/modules/secuirty_groups/main.tf
@@ -103,12 +103,12 @@ resource "aws_security_group" "app_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  } 
+  
   egress {
     from_port   = 0
     to_port     = 0
